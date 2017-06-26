62473373373446ac70e330c79bcabfe8469e2246
eloerts <emily@loertscher.org>
2017-06-26 16:14:46 -0600

    Update GenBankParser.py
    Changed input to used argparse
    Changed .txt output to tab delineated .csv file
    Added data to features on genbank output

--- python/GenBankParser.py
+++ python/GenBankParser.py
@@ -1,4 +1,4 @@
-from sys import argv
+import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -6,12 +6,18 @@ from Bio.Alphabet import IUPAC
 from Bio.SeqFeature import FeatureLocation, SeqFeature
 import random
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input GenBank file")
+parser.add_argument("-g", "--genBankOut", help="output GenBank file")
+parser.add_argument("-c", "--csvOutput", help="output text file")
+
+args = parser.parse_args()
+
 startCodons = ["ATG", "GTG", "TTG"]
 revStartCodons = ["CAT", "CAC", "CAA"]
 
 def randShiftForward(seqLoc, genomeSeq):
     upOrDown = random.randint(1,2)
-    streamShift = ""
     if upOrDown == 2:
         start = int(seqLoc.start)
         upperRange = start - 150
@@ -21,7 +27,6 @@ def randShiftForward(seqLoc, genomeSeq):
             if newStartCodon == -1:
                 newStartCodon = searchSeq.find(codon)
         newStart = upperRange + newStartCodon
-        streamShift = "up"
     else:
         start = int(seqLoc.start) + 2
         lowerRange = start + 150
@@ -31,13 +36,11 @@ def randShiftForward(seqLoc, genomeSeq):
             if newStartCodon == -1:
                 newStartCodon = searchSeq.find(codon)
         newStart = start + newStartCodon
-        streamShift = "down"
     newLoc = FeatureLocation(newStart, seqLoc.end, strand=seqLoc.strand)
-    return newLoc, streamShift
+    return newLoc
 
 def randShiftReverse(seqLoc, genomeSeq):
     upOrDown = random.randint(1,2)
-    streamShift = ""
     if upOrDown == 2:
         start = int(seqLoc.end) - 2
         upperRange = start - 150
@@ -47,7 +50,6 @@ def randShiftReverse(seqLoc, genomeSeq):
             if newStartCodon == -1:
                 newStartCodon = searchSeq.find(codon)
         newStart = upperRange + newStartCodon + 3
-        streamShift = "down"
     else:
         start = int(seqLoc.end)
         lowerRange = start + 150
@@ -57,40 +59,37 @@ def randShiftReverse(seqLoc, genomeSeq):
             if newStartCodon == -1:
                 newStartCodon = searchSeq.find(codon)
         newStart = start + newStartCodon + 3
-        streamShift = "up"
     newLoc = FeatureLocation(seqLoc.start, newStart, strand=seqLoc.strand)
-    return newLoc, streamShift
+    return newLoc
 
-def shiftDescription(oldLoc, newLoc, streamShift):
-    if oldLoc.strand == 1:
-        nucShift = abs(oldLoc.start - newLoc.start)
-        description = "{} nuclotide shift {}stream ({} => {})".format(nucShift, streamShift, oldLoc.start, newLoc.start)
-    elif oldLoc.strand == -1:
-        nucShift = abs(oldLoc.end - newLoc.end)
-        description = "{} nuclotide shift {}stream ({} => {})".format(nucShift, streamShift, oldLoc.end, newLoc.end + 1)
-    return description
-
-outputFile1 = open(argv[2], "w")
-outputFile2 = open(argv[3], "w")
-genBankFile = argv[1]
+outputFile1 = open(args.csvOutput, "w")
+outputFile2 = open(args.genBankOut, "w")
+genBankFile = args.input
 genome = SeqIO.read(genBankFile, "genbank")
 seq = genome.seq
 features = genome.features
-record = SeqRecord(seq, id = genome.id, name = genome.name)
+record = SeqRecord(seq, id = genome.id, name = genome.name, description = genome.description)
+outputFile1.write("Gene ID\tOld Start\tNew Start\n")
+newGene = None
 for feature in features:
-    if feature.type == "CDS":
+    quals = feature.qualifiers
+    if feature.type == "gene":
         local = feature.location
         geneSeq = seq[local.start:local.end]
         if local.strand == 1:
-            newGene, streamShift = randShiftForward(local, seq)
+            newGene = randShiftForward(local, seq)
         elif local.strand == -1:
-            newGene, streamShift = randShiftReverse(local, seq)
+            newGene = randShiftReverse(local, seq)
         name = str(feature.qualifiers["locus_tag"])
-        featureWO = SeqFeature(newGene, type = name)
+        featureWO = SeqFeature(newGene, type = "gene", qualifiers = quals)
         record.features.append(featureWO)
         name = name.strip("['").strip("']")
-        shiftDescript = shiftDescription(local, newGene, streamShift)
-        outputFile1.write("New {}: {}\n".format(name, shiftDescript))
+        outputFile1.write("{}\t{}\t{}\n".format(name, local.start, newGene.start))
+    elif feature.type == "CDS":
+        featureWO = SeqFeature(newGene, type = "CDS", qualifiers = quals)
+        record.features.append(featureWO)
+    else:
+        record.features.append(feature)
 SeqIO.write(record, outputFile2, "genbank")
 outputFile1.close()
 outputFile2.close()
